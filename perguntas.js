ebceeb33f62e7bbc198d406981bf2873f13a828d
jhenyfferbuenosantos-prog <jhenyffer.bueno.santos@escola.pr.gov.br>
2026-07-03 08:43:42 -0300

    jhenyffer

--- perguntas.js
+++ perguntas.js
@@ -1,16 +1,57 @@
 // Duplique o bloco criaCartao(...) abaixo para adicionar mais flash cards.
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
-riaCartao(
-   'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+criaCartao(
+   'Português',
+    'O que é uma oração subordinada? ',
+    'uma oração que depende da situação'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2'c,
-    'Resposta 2'
+    'português',
+    'oque caracteriza um texto disertativo?',
+    'a defesa de uma ideia com argumentos '
+)
+
+criaCartao(
+    'matemática',
+    'qual é a fórmula de teorema de pitágoras?',
+    'a²+b² =c² '
+)
+criaCartao(
+   'matemática',
+    'quanto é √144?',
+    '12'
+)
+
+criaCartao(
+    'filosofia',
+    'o que é ética?',
+    'o estudo da moral e das ações humanas'
+)
+
+criaCartao(
+    'filosofia',
+    'quem escreveu a república?',
+    'platão'
+)
+
+criaCartao(
+    'sociologia ',
+    'oque é cultura',
+    'conjunto de costumes,crenças e valores '
+)
+
+criaaçãoCartao(
+    'sociologia',
+    'oque é socialização',
+    'processo de integração do individuo a sociedade'
+)
+
+criaCartao(
+    'programação',
+    'o que é uma variavel',
+    'Resposta 3'
 )
 
 criaCartao(
@@ -18,16 +59,95 @@ criaCartao(
     'Pergunta 3',
     'Resposta 3'
 )
-riaCartao(
-   'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2'c,
-    'Resposta 2'
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
+)
+
+criaCartao(
+    'Disciplina 3',
+    'Pergunta 3',
+    'Resposta 3'
 )
 
 criaCartao(
